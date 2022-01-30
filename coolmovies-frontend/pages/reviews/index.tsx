8d0155cd1897d0f35807c501acafe8848126045c
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-01-30 13:40:44 -0300

    feat: Added container to movies list

--- coolmovies-frontend/pages/reviews/index.tsx
+++ coolmovies-frontend/pages/reviews/index.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 import { Header } from "../../components/Header";
 import { MovieCard } from "../../components/MovieCard";
 import { moviesActions, useAppDispatch, useAppSelector } from "../../redux";
+import { Container } from '@mui/material';
 
 type MovieResponse = {
   id: string;
@@ -35,19 +36,21 @@ const Reviews: NextPage = () => {
   return (
     <>
       <Header />
-      <div css={styles.moviesListContainer}>
-        {moviesState.fetchData?.allMovies.nodes.map((movie: MovieResponse) => (
-          <Link key={movie.id} href={`/reviews/${movie.id}`} passHref>
-            <MovieCard
-              id={movie.id}
-              title={movie.title}
-              director={movie.movieDirectorByMovieDirectorId.name}
-              releaseDate={movie.releaseDate}
-              userCreator={movie.userByUserCreatorId.name}
-            />
-          </Link>
-        ))}
-      </div>
+      <Container maxWidth="lg">
+        <div css={styles.moviesListContainer}>
+          {moviesState.fetchData?.allMovies.nodes.map((movie: MovieResponse) => (
+            <Link key={movie.id} href={`/reviews/${movie.id}`} passHref>
+              <MovieCard
+                id={movie.id}
+                title={movie.title}
+                director={movie.movieDirectorByMovieDirectorId.name}
+                releaseDate={movie.releaseDate}
+                userCreator={movie.userByUserCreatorId.name}
+              />
+            </Link>
+          ))}
+        </div>
+      </Container>
     </>
   );
 }
@@ -57,7 +60,7 @@ const styles = {
     display: flex;
     flex-direction: column;
     gap: 1rem;
-    padding: 32px;
+    padding: 1rem;
     
     @media (min-width: 768px) {
       flex-direction: row;

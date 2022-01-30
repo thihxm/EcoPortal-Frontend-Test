922b657952586a0197263e478599335cc111cb88
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2022-01-30 14:18:37 -0300

    feat: Header with links

--- coolmovies-frontend/components/Header.tsx
+++ coolmovies-frontend/components/Header.tsx
@@ -1,12 +1,22 @@
 import { css } from '@emotion/react'
 import { Paper, Typography } from "@mui/material";
+import Link from 'next/link';
 
 const primary = '#1976d2';
 
 export function Header() {
   return (
     <Paper elevation={3} css={styles.navBar}>
-      <Typography>{'EcoPortal'}</Typography>
+      <Link href="/" passHref>
+        <Typography component="a" css={styles.logo}>{'EcoPortal'}</Typography>
+      </Link>
+      <ul css={styles.menuItems}>
+        <li>
+          <Link href="/reviews" passHref>
+            <Typography component="a" css={styles.link}>{'Reviews'}</Typography>
+          </Link>
+        </li>
+      </ul>
     </Paper>
   );
 }
@@ -18,10 +28,35 @@ const styles = {
     alignSelf: 'stretch',
     display: 'flex',
     alignItems: 'center',
+    justifyContent: 'center',
     padding: 16,
     borderRadius: 0,
-    p: {
-      color: 'white',
-    },
   }),
+  logo: css`
+    color: white;
+    margin-right: auto;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
+  `,
+  menuItems: css`
+    margin-right: auto;
+    list-style: none;
+
+    display: flex;
+    gap: 1rem;
+  `,
+  link: css`
+    text-decoration: none;
+    color: white;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
+  `,
 };
